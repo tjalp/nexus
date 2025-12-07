15daa4dbd39be9b6c1ec9276edcc90923417a9c4
tjalp <60233996+tjalp@users.noreply.github.com>
2025-12-07 15:25:45 +0100

    improvement: actually okay nms

--- plugin/src/main/kotlin/net/tjalp/nexus/NexusBootstrap.kt
+++ plugin/src/main/kotlin/net/tjalp/nexus/NexusBootstrap.kt
@@ -3,13 +3,19 @@ package net.tjalp.nexus
 import io.papermc.paper.plugin.bootstrap.BootstrapContext
 import io.papermc.paper.plugin.bootstrap.PluginBootstrap
 import io.papermc.paper.plugin.bootstrap.PluginProviderContext
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents
 import org.bukkit.plugin.java.JavaPlugin
 
 @Suppress("UnstableApiUsage", "unused")
 class NexusBootstrap : PluginBootstrap {
 
     override fun bootstrap(context: BootstrapContext) {
-        
+        context.lifecycleManager.registerEventHandler(LifecycleEvents.DATAPACK_DISCOVERY) { event ->
+            val uri = javaClass.getResource("/nexus_datapack")?.toURI()
+                ?: throw IllegalStateException("Could not find nexus_datapack resource")
+
+            event.registrar().discoverPack(uri, "provided")
+        }
     }
 
     override fun createPlugin(context: PluginProviderContext): JavaPlugin = NexusPlugin
--- plugin/src/main/kotlin/net/tjalp/nexus/feature/seasons/SeasonsFeature.kt
+++ plugin/src/main/kotlin/net/tjalp/nexus/feature/seasons/SeasonsFeature.kt
@@ -1,19 +1,29 @@
 package net.tjalp.nexus.feature.seasons
 
+import io.papermc.paper.registry.RegistryAccess
+import io.papermc.paper.registry.RegistryKey
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
+import net.kyori.adventure.key.Key
+import net.minecraft.core.RegistrySynchronization
+import net.minecraft.nbt.NbtOps
 import net.minecraft.network.protocol.configuration.ClientboundRegistryDataPacket
+import net.minecraft.server.MinecraftServer
+import net.minecraft.world.level.biome.Biome
 import net.tjalp.nexus.Feature
 import net.tjalp.nexus.NexusPlugin
 import net.tjalp.nexus.util.PacketAction
 import net.tjalp.nexus.util.PacketManager
+import org.bukkit.craftbukkit.block.CraftBiome
 import org.bukkit.entity.Player
 import org.spongepowered.configurate.reactive.Disposable
+import java.util.*
+import kotlin.jvm.optionals.getOrNull
 
 object SeasonsFeature : Feature {
 
-    override val name: String = "teleport_requests"
+    override val name: String = "seasons"
 
     private var _isEnabled: Boolean = false
     override val isEnabled: Boolean
@@ -37,42 +47,29 @@ object SeasonsFeature : Feature {
         this._isEnabled = false
     }
 
-    private fun onRegistryDataPacket(packet: ClientboundRegistryDataPacket, player: Player?) : PacketAction {
-//        val registryPath = packet.registry.location().path
-//
-//        if (registryPath != "worldgen/biome") return PacketAction.Continue
-//
-//        // modify each entry to make it cold
-////        val compoundTag = CompoundTag().apply {
-////            putFloat("temperature", 0f)
-////        }
-////        val tag = FloatTag.valueOf(0f)
-//        val registry = RegistryAccess.registryAccess().getRegistry(RegistryKey.BIOME)
-//        val newEntries = packet.entries.forEach { entry ->
-//            val biome = registry.get(Key.key(entry.id.namespace, entry.id.path)) ?: return@forEach
-//            val nmsBiome = (biome as CraftBiome).handle
-//
-//            entry.data.getOrNull()?.asCompound()?.getOrNull()?.putFloat("temperature", 0f)
-//            val data = CompoundTag().apply {
-//                putByte("has_precipitation", if (nmsBiome.hasPrecipitation()) 1 else 0)
-//                putFloat("temperature", 0f)
-////                putString("temperature_modifier", nmsBiome.climateSettings.temperatureModifier)
-//                putFloat("downfall", nmsBiome.climateSettings.downfall)
-//                put("effects", nmsBiome.specialEffects)
-//            }
-////            RegistrySynchronization.PackedRegistryEntry(entry.id, Optional.of(compoundTag))
-//        }
-//
-////        newEntries.forEach { entry ->
-////            println(entry)
-////        }
-////
-////        val newPacket = ClientboundRegistryDataPacket(
-////            packet.registry,
-////
-////        )
-////
-////        return PacketAction.Replace(newPacket)
-        return PacketAction.Continue
+    private fun onRegistryDataPacket(packet: ClientboundRegistryDataPacket, player: Player?): PacketAction {
+        val registryPath = packet.registry.location().path
+
+        if (registryPath != "worldgen/biome") return PacketAction.Continue
+
+        val registry = RegistryAccess.registryAccess().getRegistry(RegistryKey.BIOME)
+        val newEntries = packet.entries.mapNotNull { entry ->
+            val biome = registry.get(Key.key(entry.id.namespace, entry.id.path)) ?: return@mapNotNull null
+            val nmsBiome = (biome as CraftBiome).handle
+            val ops = MinecraftServer.getServer().registryAccess().createSerializationContext(NbtOps.INSTANCE)
+            val tag = Biome.NETWORK_CODEC.encodeStart(ops, nmsBiome)?.result()?.getOrNull()?.asCompound()?.getOrNull()
+
+            tag?.putBoolean("has_precipitation", true)
+            tag?.putFloat("temperature", -0.7f)
+
+            RegistrySynchronization.PackedRegistryEntry(entry.id, Optional.ofNullable(tag))
+        }
+
+        val newPacket = ClientboundRegistryDataPacket(
+            packet.registry,
+            newEntries
+        )
+
+        return PacketAction.Replace(newPacket)
     }
 }
