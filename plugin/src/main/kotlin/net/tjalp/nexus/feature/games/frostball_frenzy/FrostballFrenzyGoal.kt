273543926abd5a7ca3bb673d16c7d51b3378e737
tjalp <60233996+tjalp@users.noreply.github.com>
2025-12-20 21:43:18 +0100

    improvement: finish goal

--- plugin/src/main/kotlin/net/tjalp/nexus/feature/games/frostball_frenzy/FrostballFrenzyGoal.kt
+++ plugin/src/main/kotlin/net/tjalp/nexus/feature/games/frostball_frenzy/FrostballFrenzyGoal.kt
@@ -4,6 +4,9 @@ import com.destroystokyo.paper.entity.ai.Goal
 import com.destroystokyo.paper.entity.ai.GoalKey
 import com.destroystokyo.paper.entity.ai.GoalType
 import com.destroystokyo.paper.entity.ai.GoalType.*
+import net.kyori.adventure.key.Key.key
+import net.kyori.adventure.sound.Sound
+import net.kyori.adventure.sound.Sound.sound
 import net.tjalp.nexus.NexusPlugin
 import net.tjalp.nexus.feature.games.currentGame
 import org.bukkit.NamespacedKey
@@ -11,6 +14,7 @@ import org.bukkit.entity.LivingEntity
 import org.bukkit.entity.Mob
 import org.bukkit.entity.Snowball
 import java.util.*
+import java.util.concurrent.ThreadLocalRandom
 import kotlin.math.pow
 
 class FrostballFrenzyGoal(
@@ -45,15 +49,37 @@ class FrostballFrenzyGoal(
         val distanceSquared = mob.location.distanceSquared(closestPlayer.location)
         val hasLineOfSight = mob.hasLineOfSight(closestPlayer)
 
+        mob.lookAt(closestPlayer)
+
         if (distanceSquared > 10.0.pow(2) || !hasLineOfSight) {
-            mob.pathfinder.moveTo(closestPlayer.location)
+            mob.pathfinder.moveTo(closestPlayer.location, 1.25)
         } else mob.pathfinder.stopPathfinding()
 
-//        if (closestPlayer is LivingEntity) mob.target = closestPlayer
-
         if (hasLineOfSight && distanceSquared <= 20.0.pow(2) && cooldown <= 0) {
-            val location = if (closestPlayer is LivingEntity) closestPlayer.eyeLocation else closestPlayer.location
-            mob.launchProjectile(Snowball::class.java, location.toVector().subtract(mob.eyeLocation.toVector()).normalize().multiply(1.6))
+            val random = ThreadLocalRandom.current()
+            val location = if (closestPlayer is LivingEntity) closestPlayer.eyeLocation.clone().add(0.0, 0.2, 0.0) else closestPlayer.location.clone().add(0.0, 0.2, 0.0)
+            val launchVector = location.toVector().subtract(mob.eyeLocation.toVector()).normalize()
+            val slightlyRandomizedVector = launchVector.clone().apply {
+                val spread = 0.2
+                this.x += (random.nextDouble() - 0.5) * spread
+                this.y += (random.nextDouble() - 0.5) * spread
+                this.z += (random.nextDouble() - 0.5) * spread
+                this.normalize()
+            }.multiply(1.6)
+
+            mob.launchProjectile(
+                Snowball::class.java,
+                slightlyRandomizedVector
+            )
+            mob.world.playSound(
+                sound(
+                    key("entity.snowball.throw"),
+                    Sound.Source.NEUTRAL,
+                    1f,
+                    0.4f / (random.nextFloat() * 0.4f + 0.8f)
+                ), mob
+            )
+            mob.swingMainHand()
             cooldown = 8
         }
     }
