d691932e1dca614b6694e0928c612d686eec2ea9
tjalp <60233996+tjalp@users.noreply.github.com>
2026-01-09 15:24:08 +0100

    improvement: translate teleport requests

--- plugin/src/main/kotlin/net/tjalp/nexus/command/TeleportRequestCommand.kt
+++ plugin/src/main/kotlin/net/tjalp/nexus/command/TeleportRequestCommand.kt
@@ -2,14 +2,18 @@ package net.tjalp.nexus.command
 
 import com.mojang.brigadier.Command
 import com.mojang.brigadier.context.CommandContext
+import com.mojang.brigadier.exceptions.DynamicCommandExceptionType
+import com.mojang.brigadier.exceptions.SimpleCommandExceptionType
 import com.mojang.brigadier.tree.LiteralCommandNode
 import io.papermc.paper.command.brigadier.CommandSourceStack
 import io.papermc.paper.command.brigadier.Commands.argument
 import io.papermc.paper.command.brigadier.Commands.literal
+import io.papermc.paper.command.brigadier.MessageComponentSerializer
 import io.papermc.paper.command.brigadier.argument.ArgumentTypes
 import io.papermc.paper.command.brigadier.argument.resolvers.selector.PlayerSelectorArgumentResolver
 import net.kyori.adventure.text.Component.text
-import net.kyori.adventure.text.format.NamedTextColor.RED
+import net.kyori.adventure.text.Component.translatable
+import net.kyori.adventure.text.minimessage.translation.Argument
 import net.tjalp.nexus.feature.teleportrequests.PlayerTeleportRequest
 import net.tjalp.nexus.feature.teleportrequests.TeleportRequestsFeature
 import org.bukkit.entity.Player
@@ -17,6 +21,38 @@ import org.bukkit.entity.Player
 object TeleportRequestCommand {
 
     val aliases = setOf("tprequest", "requestteleport", "requesttp", "tpr", "tpa", "tpask", "teleportask")
+
+    private val ERROR_SEND_TO_SELF = SimpleCommandExceptionType(
+        MessageComponentSerializer.message()
+            .serialize(translatable("command.teleportrequest.send.self"))
+    )
+
+    private val ERROR_DUPLICATE = DynamicCommandExceptionType { target: Any? ->
+        val name = (target as? Player)?.name() ?: text(target.toString())
+        MessageComponentSerializer.message()
+            .serialize(translatable(
+                "command.teleportrequest.send.duplicate",
+                Argument.component("target", name)
+            ))
+    }
+
+    private val ERROR_NO_REQUEST_RECEIVED = DynamicCommandExceptionType { sender: Any? ->
+        val name = (sender as? Player)?.name() ?: text(sender.toString())
+        MessageComponentSerializer.message()
+            .serialize(translatable(
+                "command.teleportrequest.accept.none",
+                Argument.component("sender", name)
+            ))
+    }
+
+    private val ERROR_NO_REQUEST_SENT = DynamicCommandExceptionType { target: Any? ->
+        val name = (target as? Player)?.name() ?: text(target.toString())
+        MessageComponentSerializer.message()
+            .serialize(translatable(
+                "command.teleportrequest.cancel.none",
+                Argument.component("target", name)
+            ))
+    }
 
     fun create(): LiteralCommandNode<CommandSourceStack> {
         return literal("teleportrequest")
@@ -41,8 +77,14 @@ object TeleportRequestCommand {
     private fun executeSend(context: CommandContext<CommandSourceStack>): Int {
         val resolver = context.getArgument("target", PlayerSelectorArgumentResolver::class.java)
         val target = resolver.resolve(context.source).first()
+        val executor = context.source.executor as Player
 
-        PlayerTeleportRequest(context.source.executor as Player, target).request()
+//        if (target == executor) throw ERROR_SEND_TO_SELF.create()
+        if (PlayerTeleportRequest.requests().any { it.source == executor && it.target == target }) {
+            throw ERROR_DUPLICATE.create(target)
+        }
+
+        PlayerTeleportRequest(executor, target).request()
 
         return Command.SINGLE_SUCCESS
     }
@@ -51,13 +93,9 @@ object TeleportRequestCommand {
         val resolver = context.getArgument("sender", PlayerSelectorArgumentResolver::class.java)
         val sender = resolver.resolve(context.source).first()
         val request = PlayerTeleportRequest.requests().find { it.source == sender && it.target == context.source.executor as Player }
+            ?: throw ERROR_NO_REQUEST_RECEIVED.create(sender)
 
-        if (request != null) {
-            request.accept()
-        } else {
-            context.source.sender.sendMessage(text("You have no active teleport request from", RED)
-                .appendSpace().append(sender.name()))
-        }
+        request.accept()
 
         return Command.SINGLE_SUCCESS
     }
@@ -66,13 +104,9 @@ object TeleportRequestCommand {
         val resolver = context.getArgument("sender", PlayerSelectorArgumentResolver::class.java)
         val sender = resolver.resolve(context.source).first()
         val request = PlayerTeleportRequest.requests().find { it.source == sender && it.target == context.source.executor as Player }
+            ?: throw ERROR_NO_REQUEST_RECEIVED.create(sender)
 
-        if (request != null) {
-            request.deny()
-        } else {
-            context.source.sender.sendMessage(text("You have no active teleport request from", RED)
-                .appendSpace().append(sender.name()))
-        }
+        request.deny()
 
         return Command.SINGLE_SUCCESS
     }
@@ -81,13 +115,9 @@ object TeleportRequestCommand {
         val resolver = context.getArgument("target", PlayerSelectorArgumentResolver::class.java)
         val target = resolver.resolve(context.source).first()
         val request = PlayerTeleportRequest.requests().find { it.source == context.source.executor as Player && it.target == target }
+            ?: throw ERROR_NO_REQUEST_SENT.create(target)
 
-        if (request != null) {
-            request.cancel()
-        } else {
-            context.source.sender.sendMessage(text("You have no active teleport request to", RED)
-                .appendSpace().append(target.name()))
-        }
+        request.cancel()
 
         return Command.SINGLE_SUCCESS
     }
--- plugin/src/main/kotlin/net/tjalp/nexus/feature/teleportrequests/PlayerTeleportRequest.kt
+++ plugin/src/main/kotlin/net/tjalp/nexus/feature/teleportrequests/PlayerTeleportRequest.kt
@@ -5,10 +5,11 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import net.kyori.adventure.key.Key
 import net.kyori.adventure.sound.Sound
-import net.kyori.adventure.text.Component.text
+import net.kyori.adventure.text.Component.*
 import net.kyori.adventure.text.event.ClickEvent.callback
 import net.kyori.adventure.text.format.NamedTextColor.*
 import net.kyori.adventure.text.format.TextDecoration.BOLD
+import net.kyori.adventure.text.minimessage.translation.Argument
 import net.tjalp.nexus.Constants.PRIMARY_COLOR
 import net.tjalp.nexus.NexusPlugin
 import org.bukkit.entity.Player
@@ -23,57 +24,54 @@ class PlayerTeleportRequest(
     private var expireJob: Job? = null
 
     override fun request() {
-        if (source == target) {
-            this.source.sendMessage(text("You cannot send a request to yourself, silly!", RED))
-            return
-        }
-        if (requests.any { it.target == target && it.source == source }) {
-            this.source.sendMessage(
-                text("You already have an active teleport request to", RED)
-                    .appendSpace().append(this.target.name().colorIfAbsent(PRIMARY_COLOR))
-            )
-            return
-        }
-
         requests += this
 
-        this.expireJob = this.scheduler.launch {
+        expireJob = this.scheduler.launch {
             delay(NexusPlugin.configuration.features.teleportRequests.requestTimeoutSeconds.seconds)
 
             requests -= this@PlayerTeleportRequest
 
-            source.sendMessage(
-                text("Your teleport request to", GRAY)
-                    .appendSpace().append(target.name().colorIfAbsent(PRIMARY_COLOR))
-                    .appendSpace().append(text("has expired"))
+            val expiredSender = translatable(
+                "teleportrequest.expired.source",
+                GRAY,
+                Argument.component("target", target.name().colorIfAbsent(PRIMARY_COLOR))
             )
-            target.sendMessage(
-                text().color(GRAY)
-                    .append(source.name().colorIfAbsent(PRIMARY_COLOR))
-                    .append(text("'s teleport request has expired"))
+            val expiredReceiver = translatable(
+                "teleportrequest.expired.target",
+                GRAY,
+                Argument.component("source", source.name().colorIfAbsent(PRIMARY_COLOR))
             )
+
+            source.sendMessage(expiredSender)
+            target.sendMessage(expiredReceiver)
         }
 
-        this.source.sendMessage(
-            text("You sent a teleport request to ", GRAY)
-                .append(this.target.name().colorIfAbsent(PRIMARY_COLOR))
-                .appendSpace().append(
-                    text("CANCEL", RED, BOLD)
-                        .clickEvent(callback { cancel() })
-                )
+        source.sendMessage(
+            translatable(
+                "teleportrequest.sent",
+                GRAY,
+                Argument.component("target", target.name().colorIfAbsent(PRIMARY_COLOR)),
+                Argument.component("cancel", translatable("teleportrequest.cancel", RED, BOLD)
+                    .clickEvent(callback { cancel() }))
+            )
         )
-        this.target.sendMessage(
-            text().color(GRAY)
-                .appendNewline()
-                .append(text("You've received a teleport request from"))
-                .appendSpace().append(this.source.name().colorIfAbsent(PRIMARY_COLOR))
-                .appendNewline().append(text("Would you like to accept this request?"))
-                .appendNewline()
-                .appendNewline().append(text("YES", GREEN, BOLD).clickEvent(callback { accept() }))
-                .appendSpace().append(text("NO", RED, BOLD).clickEvent(callback { deny() }))
-                .appendNewline()
+        target.sendMessage(
+            textOfChildren(
+                newline(),
+                translatable(
+                    "teleportrequest.received",
+                    GRAY,
+                    Argument.component("source", source.name().colorIfAbsent(PRIMARY_COLOR)),
+                    Argument.component("accept", translatable("teleportrequest.accept", GREEN, BOLD)
+                        .clickEvent(callback { accept() })),
+                    Argument.component("deny", translatable("teleportrequest.deny", RED, BOLD)
+                        .clickEvent(callback { deny() }))
+                ),
+                newline()
+            )
         )
-        this.target.playSound(
+
+        target.playSound(
             Sound.sound(Key.key("entity.item.pickup"), Sound.Source.PLAYER, 1f, 1f),
             Sound.Emitter.self()
         )
@@ -83,48 +81,56 @@ class PlayerTeleportRequest(
         if (requests.none { it.target == target && it.source == source }) return
 
         requests -= this
-        this.expireJob?.cancel()
+        expireJob?.cancel()
 
-        this.target.sendMessage(
-            text("You've accepted", GREEN)
-                .appendSpace().append(this.source.name().colorIfAbsent(PRIMARY_COLOR))
-                .append(text("'s teleport request"))
+        target.sendMessage(
+            translatable(
+                "teleportrequest.accepted.target",
+                GREEN,
+                Argument.component("source", source.name().colorIfAbsent(PRIMARY_COLOR))
+            )
         )
-        this.source.sendMessage(
-            text("Your teleport request to", GREEN)
-                .appendSpace().append(this.target.name().colorIfAbsent(PRIMARY_COLOR))
-                .appendSpace().append(text("has been accepted"))
+        source.sendMessage(
+            translatable(
+                "teleportrequest.accepted.source",
+                GREEN,
+                Argument.component("target", target.name().colorIfAbsent(PRIMARY_COLOR))
+            )
         )
 
-        val successful = this.source.teleport(this.target)
+        val successful = source.teleport(this.target)
 
         if (successful) {
             val location = this.target.location
-            this.target.world.playSound(
+            target.world.playSound(
                 Sound.sound(Key.key("item.chorus_fruit.teleport"), Sound.Source.PLAYER, 1f, 1f),
                 location.x, location.y, location.z
             )
             return
         }
 
-        this.source.sendMessage(text("Teleportation failed! Try again later", RED))
+        source.sendMessage(translatable("teleportrequest.teleport.failed", RED))
     }
 
     override fun deny() {
         if (requests.none { it.target == target && it.source == source }) return
 
         requests -= this
-        this.expireJob?.cancel()
+        expireJob?.cancel()
 
-        this.target.sendMessage(
-            text("You've denied", RED)
-                .appendSpace().append(this.source.name().colorIfAbsent(PRIMARY_COLOR))
-                .append(text("'s teleport request"))
+        target.sendMessage(
+            translatable(
+                "teleportrequest.denied.target",
+                RED,
+                Argument.component("source", source.name().colorIfAbsent(PRIMARY_COLOR))
+            )
         )
-        this.source.sendMessage(
-            text("Your teleport request to", RED)
-                .appendSpace().append(this.target.name().colorIfAbsent(PRIMARY_COLOR))
-                .appendSpace().append(text("has been denied"))
+        source.sendMessage(
+            translatable(
+                "teleportrequest.denied.source",
+                RED,
+                Argument.component("target", target.name().colorIfAbsent(PRIMARY_COLOR))
+            )
         )
     }
 
@@ -132,22 +138,27 @@ class PlayerTeleportRequest(
         if (requests.none { it.target == target && it.source == source }) return
 
         requests -= this
-        this.expireJob?.cancel()
+        expireJob?.cancel()
 
-        this.target.sendMessage(
-            text().color(GRAY)
-                .append(this.source.name().colorIfAbsent(PRIMARY_COLOR))
-                .appendSpace().append(text("has cancelled their teleport request"))
+        target.sendMessage(
+            translatable(
+                "teleportrequest.cancelled.target",
+                GRAY,
+                Argument.component("source", source.name().colorIfAbsent(PRIMARY_COLOR))
+            )
         )
-        this.source.sendMessage(
-            text("You've cancelled your teleport request to", GRAY)
-                .appendSpace().append(this.target.name().colorIfAbsent(PRIMARY_COLOR))
+        source.sendMessage(
+            translatable(
+                "teleportrequest.cancelled.source",
+                GRAY,
+                Argument.component("target", target.name().colorIfAbsent(PRIMARY_COLOR))
+            )
         )
     }
 
     override fun dispose() {
         requests -= this
-        this.expireJob?.cancel()
+        expireJob?.cancel()
     }
 
     companion object {
