d3b2c1660442be44bfaf90f4c6efe1e03cb2c7cb
tjalp <60233996+tjalp@users.noreply.github.com>
2026-02-15 17:19:18 +0100

    feat: add temporary button

---
+++ frontend/src/lib/components/ui/button/Button.svelte
@@ -0,0 +1,16 @@
+<script>
+</script>
+<!--    after:inset-shadow-[2px_2px_0_0_rgba(178, 178, 178, 0.5)] after:inset-shadow-[-2px_-2px_0_0_rgba(153, 153, 153, 0.5)]-->
+<button class="w-full gap-1.5 relative cursor-pointer select-none text-center text-white border-2 border-solid border-[#242424] bg-[#48494a]
+    after:block after:absolute after:top-0 after:left-0 after:w-full after:h-full after:z-1 after:mix-blend-hard-light after:pointer-events-none
+    active:mt-1.5 active:after:inset-shadow-[2px_2px_0_0_rgba(178,178,178,0.5),-2px_-2px_0_0_rgba(153,153,153,0.5)] active:after:bg-[#0000001a]
+    after:inset-shadow-[0_-6px_0_0_rgb(104,104,104),2px_2px_0_0_rgba(178,178,178,0.5),-2px_-8px_0_0_rgba(153,153,153,0.5)]
+    hover:not-active:after:bg-[#ffffff1a] hover:after:duration-100
+    duration-100 after:duration-100"
+>
+    <span class="flex w-[calc(100%+4px)] h-[calc(100%+4px)] -m-0.5 gap-1.5 pt-2 px-2.5 pb-3.5 text-center items-center justify-center
+        active:pt-3.5 active:pb-2 active:-mt-2 active:h-[calc(100%+10px)] duration-100"
+    >
+        Push me
+    </span>
+</button>
